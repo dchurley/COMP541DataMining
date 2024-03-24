d7c03d49678206ee2b0060cfde2365afd18d3e40
dchurley <136537343+dchurley@users.noreply.github.com>
2024-03-23 23:09:31 -0700

    working on sentiment analysis for better classification

--- R/Database.R
+++ R/Database.R
@@ -1,9 +1,27 @@
 library(RMySQL)
-mysqlconnection = dbConnect(RMySQL::MySQL(),
-                            dbname='project',
-                            host='150.230.44.118',
-                            port=3306,
-                            user='project',
-                            password='COMP541',
-                            local_infile = TRUE)
-dbListTables(mysqlconnection)
+
+
+SQL_GET_DATA <- function(table, where = "") {
+  
+  
+  conn = dbConnect(RMySQL::MySQL(),
+                              dbname='project',
+                              host='150.230.44.118',
+                              port=3306,
+                              user='project',
+                              password='COMP541',
+                              local_infile = TRUE)
+  
+  
+  query <- paste0("SELECT * FROM `", table)
+  
+  if(where != "")
+    query <- paste0(query, "` WHERE ", where)
+  else
+    query <- paste0(query, "`")
+  
+  data <- dbGetQuery(conn, statement = query)
+  
+  return(data)
+  
+}
