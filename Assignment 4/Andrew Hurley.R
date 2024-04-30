a3a3768ece2b8b4a1788ead9c3363ef4e5295859
dchurley <136537343+dchurley@users.noreply.github.com>
2024-04-29 20:58:15 -0700

    f

--- Assignment 4/Andrew Hurley.R
+++ Assignment 4/Andrew Hurley.R
@@ -5,8 +5,11 @@ library(arules)
 #Load Groceries
 data("Groceries")
 
+#Ensure dataset is correct (9835 rows)
+print(Groceries)
+
 # Run Apriori algorithm
 rules <- apriori(Groceries, parameter = list(supp = 0.02, conf = 0.2))
 
-# Display the top 10 rules
+# Print top 10 rules
 inspect(head(rules, n = 10))
