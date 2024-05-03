1af129c57be94963f1b2326ef5f7f7939a1f8803
dchurley <136537343+dchurley@users.noreply.github.com>
2024-05-03 12:58:18 -0700

    updates

--- R/Normalize Row.R
+++
@@ -1,9 +0,0 @@
-data <- data.frame(column1 = c(2, 2, 4),
-                   column2 = c(2, 2, 4),
-                   column3 = c(4, 4, 4))
-
-data
-
-data_percent <- t(apply(data, 1, function(x) x / sum(x)))
-
-data_percent
